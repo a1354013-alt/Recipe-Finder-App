4d8d1c05e2dedb5779cfc814af6a0db2cefb2305
YangWen Luo <a1354013@gm.ncue.edu.tw>
2026-04-15 10:53:16 +0800

    Recipe-Finder-App_v2.89

--- client/src/hooks/usePersistFn.ts
+++ client/src/hooks/usePersistFn.ts
@@ -1,20 +1,22 @@
 import { useRef } from "react";
 
-type noop = (...args: unknown[]) => unknown;
-
 /**
- * usePersistFn instead of useCallback to reduce cognitive load
+ * Return a stable function reference while always calling the latest fn.
  */
-export function usePersistFn<T extends noop>(fn: T) {
-  const fnRef = useRef<T>(fn);
+export function usePersistFn<T extends (...args: any[]) => any>(fn: T): T {
+  const fnRef = useRef(fn);
   fnRef.current = fn;
 
-  const persistFn = useRef<T>(null);
-  if (!persistFn.current) {
-    persistFn.current = function (this: unknown, ...args) {
-      return fnRef.current!.apply(this, args);
+  const persistFnRef = useRef<T | null>(null);
+
+  if (!persistFnRef.current) {
+    persistFnRef.current = function (
+      this: ThisParameterType<T>,
+      ...args: Parameters<T>
+    ): ReturnType<T> {
+      return fnRef.current.apply(this, args);
     } as T;
   }
 
-  return persistFn.current!;
+  return persistFnRef.current;
 }
